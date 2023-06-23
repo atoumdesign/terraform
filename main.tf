749e4d760ea8f5df1b6b90dceb65456c2c871ed0
atoumdesign <34277797+atoumdesign@users.noreply.github.com>
2023-06-22 22:41:45 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "resource_group" {
   for_each = var.resource
 
-  name      = "rg-${terraform.workspace}-${var.application}${each.value.name}${var.location}-${each.value.number}"
-  location  = var.location
+  name      = "rg-${terraform.workspace}-${var.generals.application}${each.value.name}${var.generals.location}-${each.value.number}"
+  location  = var.generals.location
   tags      = var.tags
 }
