cadffdc14c48e10219f6e6c302b6cae81d511b85
atoumdesign <34277797+atoumdesign@users.noreply.github.com>
2023-06-22 22:36:41 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,32 +1,17 @@
-variable "rg" {
+variable "resource_group" {
   type = map(object({
     name = string
     number = string
   }))
 }
 
+variable "generals" {
+  type = map(object({
+    application = string
+    location = string
+  }))
+}
+
 variable "tags" {
   type = map(string)
-}
-
-variable "application" {
-  type = string
-}
-
-variable "location" {
-  type = string
-  default = "brazilsouth"
-
-  validation {
-    condition = contains([
-        "brazilsouth",
-        "eastus"
-        ], var.location)
-
-    error_message = "Only brazilsouth and eastus locations are allowed"
-  }
-}
-
-variable "environment" {
-  type = string
 }
